cf661e63369380598e6242c4eef47a4940a72029
zhuminghui17 <zhuminghui17@gmail.com>
2025-07-05 13:49:50 -0400

    feat(question): add dashboard question and question view pages
    - Created a new question listing page with metadata and layout.
    - Implemented a question view page to display individual question details.
    - Updated button component for consistent styling across the application.

--- src/app/dashboard/question/[questionId]/page.tsx
+++ src/app/dashboard/question/[questionId]/page.tsx
@@ -1,13 +1,13 @@
 import FormCardSkeleton from '@/components/form-card-skeleton';
 import PageContainer from '@/components/layout/page-container';
 import { Suspense } from 'react';
-import ProductViewPage from '@/features/products/components/product-view-page';
+import QuestionViewPage from '@/features/question/components/question-view-page';
 
 export const metadata = {
-  title: 'Dashboard : Product View'
+  title: 'Dashboard : Question View'
 };
 
-type PageProps = { params: Promise<{ productId: string }> };
+type PageProps = { params: Promise<{ questionId: string }> };
 
 export default async function Page(props: PageProps) {
   const params = await props.params;
@@ -15,7 +15,7 @@ export default async function Page(props: PageProps) {
     <PageContainer scrollable>
       <div className='flex-1 space-y-4'>
         <Suspense fallback={<FormCardSkeleton />}>
-          <ProductViewPage productId={params.productId} />
+          <QuestionViewPage questionId={params.questionId} />
         </Suspense>
       </div>
     </PageContainer>
--- src/app/dashboard/question/page.tsx
+++ src/app/dashboard/question/page.tsx
@@ -3,7 +3,7 @@ import { buttonVariants } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 import { DataTableSkeleton } from '@/components/ui/table/data-table-skeleton';
-import ProductListingPage from '@/features/products/components/product-listing';
+import QuestionListingPage from '@/features/question/components/question-listing';
 import { searchParamsCache, serialize } from '@/lib/searchparams';
 import { cn } from '@/lib/utils';
 import { IconPlus } from '@tabler/icons-react';
@@ -12,7 +12,7 @@ import { SearchParams } from 'nuqs/server';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Dashboard: Products'
+  title: 'Dashboard: Question'
 };
 
 type pageProps = {
@@ -31,12 +31,9 @@ export default async function Page(props: pageProps) {
     <PageContainer scrollable={false}>
       <div className='flex flex-1 flex-col space-y-4'>
         <div className='flex items-start justify-between'>
-          <Heading
-            title='Products'
-            description='Manage products (Server side table functionalities.)'
-          />
+          <Heading title='Question' description='Browse past questions' />
           <Link
-            href='/dashboard/product/new'
+            href='/dashboard/question/new'
             className={cn(buttonVariants(), 'text-xs md:text-sm')}
           >
             <IconPlus className='mr-2 h-4 w-4' /> Add New
@@ -49,7 +46,7 @@ export default async function Page(props: pageProps) {
             <DataTableSkeleton columnCount={5} rowCount={8} filterCount={2} />
           }
         >
-          <ProductListingPage />
+          <QuestionListingPage />
         </Suspense>
       </div>
     </PageContainer>
